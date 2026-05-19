1b99f70e4077eba063fca44f9b85e9a42dc72ce6
Hackershashank <shashanksingh14370@gmail.com>
2026-05-19 14:01:58 +0530

    added repository annotation

--- demo/src/main/java/com/example/demo/repository/AdminRepository.java
+++ demo/src/main/java/com/example/demo/repository/AdminRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.Admin;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdminRepository extends JpaRepository<Admin,Long> {
 
 }
