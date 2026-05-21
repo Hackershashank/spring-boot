e6f43da0d3f6d24befe95f9e04e74d864c9ebb2a
Hackershashank <shashanksingh14370@gmail.com>
2026-05-21 12:55:51 +0530

    Added validation exception

--- demo/src/main/java/com/example/demo/exception/ApiError.java
+++ demo/src/main/java/com/example/demo/exception/ApiError.java
@@ -5,16 +5,16 @@ import java.time.LocalDateTime;
 public class ApiError {
     private LocalDateTime timestamp;
     private int status;
-    private String message;
+    private Object errors;
 
-    public ApiError(int status, String message){
+    public ApiError(int status, Object errors){
         this.timestamp=LocalDateTime.now();
         this.status=status;
-        this.message=message;
+        this.errors=errors;
     }
 
     public LocalDateTime getTimestamp(){ return timestamp; }
     public int getStatus(){ return status; }
-    public String getMessage(){ return message; }
+    public Object getMessage(){ return errors; }
 
 }
--- demo/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ demo/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.demo.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,12 +15,43 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    // Resource Not Found Exception
     @ExceptionHandler(ResourceNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ResponseBody
+    public ResponseEntity<ApiError> handleResourceNotFound(ResourceNotFoundException ex){
+        ApiError error=new ApiError(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage()
+        );
+        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+    }
 
-    public ApiError handleNotFound(ResourceNotFoundException ex){
-        return new ApiError(404, ex.getMessage());
+    // Validation Errors
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleValidationErrors(MethodArgumentNotValidException ex){
+        Map<String,String> errors=new HashMap<>();
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error->
+                        errors.put(
+                                error.getField(),
+                                error.getDefaultMessage()
+                        )
+                );
+        ApiError apiError=new ApiError(
+                HttpStatus.BAD_REQUEST.value(),
+                errors
+        );
+        return new ResponseEntity<>(apiError,HttpStatus.BAD_REQUEST);
+    }
+
+    //Generic Exception
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleGenericException(Exception ex){
+        ApiError error=new ApiError(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                ex.getMessage()
+        );
+        return new ResponseEntity<>(error,HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
